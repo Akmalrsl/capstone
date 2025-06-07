a6dad847935cc3a9cfd8af024a32497a84641979
akmalrosli545 <akmalrosli545@gmail.com>
2025-06-07 22:48:07 +0900

    Latest update untuk baca dari 'readings' table Delete file get_ecgaverage pastu tukar jadi get_reading_values

---
+++ get_reading_values.php
@@ -0,0 +1,15 @@
+<?php
+include 'db_connect.php';
+
+if (isset($_GET['id'])) {
+    $id = intval($_GET['id']);
+    $query = "SELECT ecgAverage, sbp, dbp FROM readings WHERE id = $id LIMIT 1";
+    $result = $health_conn->query($query);
+
+    if ($row = $result->fetch_assoc()) {
+        echo json_encode($row); // return all 3 values as JSON
+    } else {
+        echo json_encode(["ecgAverage" => 0, "sbp" => 0, "dbp" => 0]);
+    }
+}
+?>
